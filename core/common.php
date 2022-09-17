1af04b8fd1d7842e3a46abd12d8843511f5404a5
ICIO_XieDing <57896269+A-Lost-Kid@users.noreply.github.com>
2022-09-17 19:40:44 +0800

    Create common.php

---
+++ core/common.php
@@ -0,0 +1,21 @@
+<?php
+//error_reporting(E_ALL);
+define('IN_CRONLITE', true);
+define('SYSTEM_ROOT', dirname(__FILE__).'/');
+define('ROOT', dirname(SYSTEM_ROOT).'/');
+define('SYS_KEY', 'EQUWRUQWOY');
+date_default_timezone_set("PRC");
+$date = date("Y-m-d H:i:s");
+session_start();
+$scriptpath=str_replace('\\','/',$_SERVER['SCRIPT_NAME']);
+$sitepath = substr($scriptpath, 0, strrpos($scriptpath, '/'));
+$siteurl = ($_SERVER['SERVER_PORT'] == '443' ? 'https://' : 'http://').$_SERVER['HTTP_HOST'].$sitepath.'/';
+require ROOT.'config.php';
+//连接数据库
+include_once(SYSTEM_ROOT."db.class.php");
+$DB=new DB($dbconfig['host'],$dbconfig['user'],$dbconfig['pwd'],$dbconfig['dbname'],$dbconfig['port']);
+	
+$password_hash='!@#%!s!0';
+include_once(SYSTEM_ROOT."function.php");
+set_time_limit(99999);
+?>
